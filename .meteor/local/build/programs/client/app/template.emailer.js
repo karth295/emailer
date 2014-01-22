7af4500ad51b49fb7f482ef58698777b63459af9
Karthik Palaniappan <karth295@attu3.cs.washington.edu>
2014-01-21 17:09:25 -0800

    Deleted old stuff

--- .meteor/local/build/programs/client/app/template.emailer.js
+++
@@ -1,3 +0,0 @@
-(function(){Meteor.startup(function(){document.body.appendChild(Spark.render(Template.__define__(null,Package.handlebars.Handlebars.json_ast_to_func([[">","hello"]]))));});Template.__define__("hello",Package.handlebars.Handlebars.json_ast_to_func(["<h1>Hello World!</h1>\n  <textarea id=\"data\"></textarea>\n  <button id=\"submit\">Submit</button>"]));
-
-})();
